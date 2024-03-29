1e8a86487b376a17a20dbe4cb6725e75ac66ac7e
davidgikungu <157691878+davidgikungu@users.noreply.github.com>
2024-03-29 18:31:50 +0300

    Add files via upload
    The projects involves an introduction into loops .

---
+++ dowm.py
@@ -0,0 +1,4 @@
+for j in range(1,8):
+    for i in range(1,j):
+        print("",i,end="")
+    print("\n")
---
+++ patterna.py
@@ -0,0 +1,14 @@
+
+print("kilograms")
+for j in range(1,200):
+    print(j,"/",end="")
+    print("\n")
+i=1
+while i<=199:
+    print(i*2.2)
+    i+=1
+
+
+
+
+
